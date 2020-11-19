788df037eafd4268ab47fd751e25e63abf88b7b0
cs18b047 <cs18b047@smail.iitm.ac.in>
2020-11-19 21:43:39 +0530

    added sched.c

--- xv6-riscv/kernel/defs.h
+++ xv6-riscv/kernel/defs.h
@@ -182,5 +182,10 @@ void            virtio_disk_init(void);
 void            virtio_disk_rw(struct buf *, int);
 void            virtio_disk_intr();
 
+// sched.c
+void  			sched_init(void);
+void 			sched_insert(struct proc*,int);
+struct proc*    sched_get();
+
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
--- xv6-riscv/kernel/main.c
+++ xv6-riscv/kernel/main.c
@@ -28,6 +28,7 @@ main()
     iinit();         // inode cache
     fileinit();      // file table
     virtio_disk_init(); // emulated hard disk
+    sched_init();	 // Initialising scheduler
     userinit();      // first user process
     __sync_synchronize();
     started = 1;
